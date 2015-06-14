00cd4cd58d602035a71d6324f55cff2d8199ac91
Palli Moon <pallimoon@gmail.com>
2015-06-14 16:16:02 +0000

    Add zsh and oh my zsh to provisions.sh

--- provisions.sh
+++ provisions.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y git g++ zsh
+echo "********************************"
+echo "      Provision script for      "
+echo "    Ubuntu Trusty Tahr 64bit    "
+echo "         by Palli Moon          "
+echo "********************************"
+
+apt-get update -q
+apt-get install -yq git g++ zsh
+
+# Install zsh with oh my zsh
+if [ ! -d ~vagrant/.oh-my-zsh ]; then
+  git clone https://github.com/robbyrussell/oh-my-zsh.git ~vagrant/.oh-my-zsh
+fi
+cp ~vagrant/.oh-my-zsh/templates/zshrc.zsh-template ~vagrant/.zshrc
+chown vagrant: ~vagrant/.zshrc
+chsh -s /bin/zsh vagrant
