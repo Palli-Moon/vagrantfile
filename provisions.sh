618066c7f9ee98602473287a190ae1336fc81fe7
Palli Moon <pallimoon@gmail.com>
2015-06-26 05:51:47 +0000

    Remove screenfetch, add echo signature

--- provisions.sh
+++ provisions.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-echo "********************************"
-echo "      Provision script for      "
-echo "    Ubuntu Trusty Tahr 64bit    "
-echo "         by Palli Moon          "
-echo "********************************"
+echo "******************************************"
+echo "Provision script by Palli Moon"
+echo "******************************************"
 
 add-apt-repository ppa:djcj/screenfetch
 apt-get update
-apt-get install -y git g++ zsh sl tree screenfetch keychain
+apt-get install -y git g++ zsh sl tree keychain
 
 # Install zsh with oh my zsh
 if [ ! -d ~vagrant/.oh-my-zsh ]; then
@@ -20,11 +18,6 @@ chsh -s /bin/zsh vagrant
 
 # Clone dotfiles repository into user directory
 git clone https://github.com/Palli-Moon/dotfiles.git ~vagrant/dotfiles
-
-# Run screenfetch on startup/ssh
-#echo "clear" >> ~vagrant/.zshrc
-#echo "if [ -f /usr/bin/screenfetch ]; then screenfetch; fi" >> ~vagrant/.zshrc
-#echo "echo \n\n" >> ~vagrant/.zshrc
 
 # Start keychain on ssh
 echo 'eval `keychain --eval --agents ssh id_rsa id_dsa`' >> ~vagrant/.zshrc
